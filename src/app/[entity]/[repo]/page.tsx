a858b01d0cfc56d79898e126f80e88553260f40a
Bryce Jackson <bryce@brycedev.com>
2023-03-05 17:47:53 -0500

    add links

--- src/app/[entity]/[repo]/page.tsx
+++ src/app/[entity]/[repo]/page.tsx
@@ -41,11 +41,11 @@ export default function RepoPage({
         <div className="flex justify-between">
           <div className="flex items-center text-lg">
             <Book className="mr-2 inline h-4 w-4 text-gray-400" />
-            <Link className="text-purple-500" href="#">
+            <Link className="text-purple-500" href={`/${params.entity}`}>
               { params.entity }
             </Link>
             <span className="px-2 text-gray-400">/</span>
-            <Link className="text-purple-500" href="#">
+            <Link className="text-purple-500" href={`/${params.entity}/${params.repo}`}>
               { params.repo }
             </Link>
             <span className="ml-1.5 mt-px rounded-full border border-gray-200/40 px-1.5 text-xs text-gray-400">
