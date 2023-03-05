1fe2c639f5cd89883b49f66bba666943a7207d1d
Peer Richelsen <peer@cal.com>
2023-03-05 16:46:45 +0100

    fixed collaborators avatar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -262,184 +262,160 @@ const Home: NextPage = () => {
                 </h3>
 
                 <div>
-                  <ul className="grid grid-cols-7">
-                    <li className="">
-                      <a
-                        href="https://github.com/fiatjaf"
-                        className=""
-                        data-hovercard-type="user"
-                        data-hovercard-url="/users/fiatjaf/hovercard"
-                        data-octo-click="hovercard-link-click"
-                        data-octo-dimensions="link_type:self"
-                      >
-                        <img
-                          src="https://avatars.githubusercontent.com/u/1653275?s=64&amp;v=4"
-                          alt="@fiatjaf"
-                          className="h-8 w-8 rounded-full"
-                        />
-                      </a>
-                    </li>
-                    <li className="mb-2 mr-2">
-                      <a
-                        href="https://github.com/scsibug"
-                        className=""
-                        data-hovercard-type="user"
-                        data-hovercard-url="/users/scsibug/hovercard"
-                        data-octo-click="hovercard-link-click"
-                        data-octo-dimensions="link_type:self"
-                      >
-                        <img
-                          src="https://avatars.githubusercontent.com/u/57707?s=64&amp;v=4"
-                          alt="@scsibug"
-                          className="h-8 w-8 rounded-full"
-                        />
-                      </a>
-                    </li>
-                    <li className="mb-2 mr-2">
-                      <a
-                        href="https://github.com/melvincarvalho"
-                        className=""
-                        data-hovercard-type="user"
-                        data-hovercard-url="/users/melvincarvalho/hovercard"
-                        data-octo-click="hovercard-link-click"
-                        data-octo-dimensions="link_type:self"
-                      >
-                        <img
-                          src="https://avatars.githubusercontent.com/u/65864?s=64&amp;v=4"
-                          alt="@melvincarvalho"
-                          className="h-8 w-8 rounded-full"
-                        />
-                      </a>
-                    </li>
-                    <li className="mb-2 mr-2">
-                      <a
-                        href="https://github.com/hieblmi"
-                        className=""
-                        data-hovercard-type="user"
-                        data-hovercard-url="/users/hieblmi/hovercard"
-                        data-octo-click="hovercard-link-click"
-                        data-octo-dimensions="link_type:self"
-                      >
-                        <img
-                          src="https://avatars.githubusercontent.com/u/18631614?s=64&amp;v=4"
-                          alt="@hieblmi"
-                          className="h-8 w-8 rounded-full"
-                        />
-                      </a>
-                    </li>
-                    <li className="mb-2 mr-2">
-                      <a
-                        href="https://github.com/srid"
-                        className=""
-                        data-hovercard-type="user"
-                        data-hovercard-url="/users/srid/hovercard"
-                        data-octo-click="hovercard-link-click"
-                        data-octo-dimensions="link_type:self"
-                      >
-                        <img
-                          src="https://avatars.githubusercontent.com/u/3998?s=64&amp;v=4"
-                          alt="@srid"
-                          className="h-8 w-8 rounded-full"
-                        />
-                      </a>
-                    </li>
-                    <li className="mb-2 mr-2">
-                      <a
-                        href="https://github.com/asoltys"
-                        className=""
-                        data-hovercard-type="user"
-                        data-hovercard-url="/users/asoltys/hovercard"
-                        data-octo-click="hovercard-link-click"
-                        data-octo-dimensions="link_type:self"
-                      >
-                        <img
-                          src="https://avatars.githubusercontent.com/u/7641?s=64&amp;v=4"
-                          alt="@asoltys"
-                          className="h-8 w-8 rounded-full"
-                        />
-                      </a>
-                    </li>
-                    <li className="mb-2 mr-2">
-                      <a
-                        href="https://github.com/jb55"
-                        className=""
-                        data-hovercard-type="user"
-                        data-hovercard-url="/users/jb55/hovercard"
-                        data-octo-click="hovercard-link-click"
-                        data-octo-dimensions="link_type:self"
-                      >
-                        <img
-                          src="https://avatars.githubusercontent.com/u/45598?s=64&amp;v=4"
-                          alt="@jb55"
-                          className="h-8 w-8 rounded-full"
-                        />
-                      </a>
-                    </li>
-                    <li className="mb-2 mr-2">
-                      <a
-                        href="https://github.com/schulterklopfer"
-                        className=""
-                        data-hovercard-type="user"
-                        data-hovercard-url="/users/schulterklopfer/hovercard"
-                        data-octo-click="hovercard-link-click"
-                        data-octo-dimensions="link_type:self"
-                      >
-                        <img
-                          src="https://avatars.githubusercontent.com/u/397451?s=64&amp;v=4"
-                          alt="@schulterklopfer"
-                          className="h-8 w-8 rounded-full"
-                        />
-                      </a>
-                    </li>
-                    <li className="mb-2 mr-2">
-                      <a
-                        href="https://github.com/GlenCooper"
-                        className=""
-                        data-hovercard-type="user"
-                        data-hovercard-url="/users/GlenCooper/hovercard"
-                        data-octo-click="hovercard-link-click"
-                        data-octo-dimensions="link_type:self"
-                      >
-                        <img
-                          src="https://avatars.githubusercontent.com/u/967793?s=64&amp;v=4"
-                          alt="@GlenCooper"
-                          className="h-8 w-8 rounded-full"
-                        />
-                      </a>
-                    </li>
-                    <li className="mb-2 mr-2">
-                      <a
-                        href="https://github.com/Kukks"
-                        className=""
-                        data-hovercard-type="user"
-                        data-hovercard-url="/users/Kukks/hovercard"
-                        data-octo-click="hovercard-link-click"
-                        data-octo-dimensions="link_type:self"
-                      >
-                        <img
-                          src="https://avatars.githubusercontent.com/u/1818366?s=64&amp;v=4"
-                          alt="@Kukks"
-                          className="h-8 w-8 rounded-full"
-                        />
-                      </a>
-                    </li>
-                    <li className="mb-2 mr-2">
-                      <a
-                        href="https://github.com/verretor"
-                        className=""
-                        data-hovercard-type="user"
-                        data-hovercard-url="/users/verretor/hovercard"
-                        data-octo-click="hovercard-link-click"
-                        data-octo-dimensions="link_type:self"
-                      >
-                        <img
-                          src="https://avatars.githubusercontent.com/u/10324237?s=64&amp;v=4"
-                          alt="@verretor"
-                          className="h-8 w-8 rounded-full"
-                        />
-                      </a>
-                    </li>
-                  </ul>
+                  <a
+                    href="https://github.com/fiatjaf"
+                    className="mr-2 inline-block h-8 w-8 rounded-full"
+                    data-hovercard-type="user"
+                    data-hovercard-url="/users/fiatjaf/hovercard"
+                    data-octo-click="hovercard-link-click"
+                    data-octo-dimensions="link_type:self"
+                  >
+                    <img
+                      src="https://avatars.githubusercontent.com/u/1653275?s=64&amp;v=4"
+                      alt="@fiatjaf"
+                      className="rounded-full"
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/scsibug"
+                    className="mr-2 inline-block h-8 w-8 rounded-full"
+                    data-hovercard-type="user"
+                    data-hovercard-url="/users/scsibug/hovercard"
+                    data-octo-click="hovercard-link-click"
+                    data-octo-dimensions="link_type:self"
+                  >
+                    <img
+                      src="https://avatars.githubusercontent.com/u/57707?s=64&amp;v=4"
+                      alt="@scsibug"
+                      className="rounded-full"
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/melvincarvalho"
+                    className="mr-2 inline-block h-8 w-8 rounded-full"
+                    data-hovercard-type="user"
+                    data-hovercard-url="/users/melvincarvalho/hovercard"
+                    data-octo-click="hovercard-link-click"
+                    data-octo-dimensions="link_type:self"
+                  >
+                    <img
+                      src="https://avatars.githubusercontent.com/u/65864?s=64&amp;v=4"
+                      alt="@melvincarvalho"
+                      className="rounded-full"
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/hieblmi"
+                    className="mr-2 inline-block h-8 w-8 rounded-full"
+                    data-hovercard-type="user"
+                    data-hovercard-url="/users/hieblmi/hovercard"
+                    data-octo-click="hovercard-link-click"
+                    data-octo-dimensions="link_type:self"
+                  >
+                    <img
+                      src="https://avatars.githubusercontent.com/u/18631614?s=64&amp;v=4"
+                      alt="@hieblmi"
+                      className="rounded-full"
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/srid"
+                    className="mr-2 inline-block h-8 w-8 rounded-full"
+                    data-hovercard-type="user"
+                    data-hovercard-url="/users/srid/hovercard"
+                    data-octo-click="hovercard-link-click"
+                    data-octo-dimensions="link_type:self"
+                  >
+                    <img
+                      src="https://avatars.githubusercontent.com/u/3998?s=64&amp;v=4"
+                      alt="@srid"
+                      className="rounded-full"
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/asoltys"
+                    className="mr-2 inline-block h-8 w-8 rounded-full"
+                    data-hovercard-type="user"
+                    data-hovercard-url="/users/asoltys/hovercard"
+                    data-octo-click="hovercard-link-click"
+                    data-octo-dimensions="link_type:self"
+                  >
+                    <img
+                      src="https://avatars.githubusercontent.com/u/7641?s=64&amp;v=4"
+                      alt="@asoltys"
+                      className="rounded-full"
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/jb55"
+                    className="mr-2 inline-block h-8 w-8 rounded-full"
+                    data-hovercard-type="user"
+                    data-hovercard-url="/users/jb55/hovercard"
+                    data-octo-click="hovercard-link-click"
+                    data-octo-dimensions="link_type:self"
+                  >
+                    <img
+                      src="https://avatars.githubusercontent.com/u/45598?s=64&amp;v=4"
+                      alt="@jb55"
+                      className="rounded-full"
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/schulterklopfer"
+                    className="mr-2 inline-block h-8 w-8 rounded-full"
+                    data-hovercard-type="user"
+                    data-hovercard-url="/users/schulterklopfer/hovercard"
+                    data-octo-click="hovercard-link-click"
+                    data-octo-dimensions="link_type:self"
+                  >
+                    <img
+                      src="https://avatars.githubusercontent.com/u/397451?s=64&amp;v=4"
+                      alt="@schulterklopfer"
+                      className="rounded-full"
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/GlenCooper"
+                    className="mr-2 inline-block h-8 w-8 rounded-full"
+                    data-hovercard-type="user"
+                    data-hovercard-url="/users/GlenCooper/hovercard"
+                    data-octo-click="hovercard-link-click"
+                    data-octo-dimensions="link_type:self"
+                  >
+                    <img
+                      src="https://avatars.githubusercontent.com/u/967793?s=64&amp;v=4"
+                      alt="@GlenCooper"
+                      className="rounded-full"
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/Kukks"
+                    className="mr-2 inline-block h-8 w-8 rounded-full"
+                    data-hovercard-type="user"
+                    data-hovercard-url="/users/Kukks/hovercard"
+                    data-octo-click="hovercard-link-click"
+                    data-octo-dimensions="link_type:self"
+                  >
+                    <img
+                      src="https://avatars.githubusercontent.com/u/1818366?s=64&amp;v=4"
+                      alt="@Kukks"
+                      className="rounded-full"
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/verretor"
+                    className="mr-2 inline-block h-8 w-8 rounded-full"
+                    data-hovercard-type="user"
+                    data-hovercard-url="/users/verretor/hovercard"
+                    data-octo-click="hovercard-link-click"
+                    data-octo-dimensions="link_type:self"
+                  >
+                    <img
+                      src="https://avatars.githubusercontent.com/u/10324237?s=64&amp;v=4"
+                      alt="@verretor"
+                      className="rounded-full"
+                    />
+                  </a>
                 </div>
               </div>
             </aside>
