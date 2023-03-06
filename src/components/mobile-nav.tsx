f352b79567060857af8080719a82262ef7fa285e
Bruce Wang <88688323+BruceWangyq@users.noreply.github.com>
2023-03-05 19:55:55 -0500

    fix: change hooks file dir

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Link from "next/link";
-import { useLockBody } from "../../hooks/use-lock-body";
+import { useLockBody } from "../hooks/use-lock-body";
 
 import { MainNavItem } from "types";
 import { cn } from "@/lib/utils";
