eda9619499bc50e6e12657883e102c6b4209b44e
pierresisson <79455860+pierresisson@users.noreply.github.com>
2023-03-06 20:25:43 +0100

    Squash feature/add-mobile-layout-step-1 (merge-base 1050730ed4ab88134e11149e17d4dcd53dd980ba, previous tip 48adbbccc908ccb8fe1a5dc65524ec3675452635)
    commit 48adbbccc908ccb8fe1a5dc65524ec3675452635
    Author: pierresisson <79455860+pierresisson@users.noreply.github.com>
    Date:   Mon Mar 6 20:10:46 2023 +0100
    
        add gap top banner
    
    commit e1577ebc8fd62aa9748dd8eabb95cc8c44bc58b3
    Author: pierresisson <79455860+pierresisson@users.noreply.github.com>
    Date:   Mon Mar 6 19:09:59 2023 +0100
    
        fix conflicts
    
    commit 5d5b94c9fc452c47a47c6b3d3f68db9a0adb111b
    Author: pierresisson <79455860+pierresisson@users.noreply.github.com>
    Date:   Mon Mar 6 18:57:31 2023 +0100
    
        fix conflicts
    
    commit fc0a7ea7e080507d8f1a64395d3bd8880157992b
    Author: pierresisson <79455860+pierresisson@users.noreply.github.com>
    Date:   Mon Mar 6 18:53:59 2023 +0100
    
        Squash feature/add-mobile-layout-step-1 (merge-base 1050730ed4ab88134e11149e17d4dcd53dd980ba, previous tip 116045965fb15f26cc1834378bd1d0524c807968)
    
        commit 116045965fb15f26cc1834378bd1d0524c807968
        Author: pierresisson <79455860+pierresisson@users.noreply.github.com>
        Date:   Mon Mar 6 18:50:33 2023 +0100
    
            Squash feature/add-mobile-layout-step-1 (merge-base 3a1481de44c52e8e79652ac5e8c5d7ebf4e8e810, previous tip be1a4f98a4e10be2abf5dae0b17467815aa92f72)
    
            commit be1a4f98a4e10be2abf5dae0b17467815aa92f72
            Author: pierresisson <79455860+pierresisson@users.noreply.github.com>
            Date:   Mon Mar 6 17:32:47 2023 +0100
    
                remove yarnlock
    
            commit 70d1015fffe20f4cc7d10d5992df09764bb01fe7
            Merge: e957706 3a1481d
            Author: pierresisson <79455860+pierresisson@users.noreply.github.com>
            Date:   Mon Mar 6 17:31:41 2023 +0100
    
                Merge branch 'main' into feature/add-mobile-layout-step-1
    
            commit e957706501e0babcf9c53f3036e2b40dd0548206
            Author: pierresisson <79455860+pierresisson@users.noreply.github.com>
            Date:   Mon Mar 6 17:29:11 2023 +0100
    
                improve layout
    
            commit 525fd9d14944b3efd2daeed91ea79d4d39171323
            Author: pierresisson <79455860+pierresisson@users.noreply.github.com>
            Date:   Mon Mar 6 10:37:45 2023 +0100
    
                improve layout
    
            commit 387afb8d49196dbe186c5f3d7195600a0d02e78c
            Author: pierresisson <79455860+pierresisson@users.noreply.github.com>
            Date:   Mon Mar 6 09:25:12 2023 +0100
    
                Add mobile layout first step
    
            commit ddb8c0eb71a6eaccb284f66cefb46f8d6a2d24cf
            Author: pierresisson <79455860+pierresisson@users.noreply.github.com>
            Date:   Mon Mar 6 09:16:38 2023 +0100
    
                Add mobile layout first step

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="dark">
-        <div
-          style={{ minWidth: 1080 }}
-          className="dark min-h-screen bg-white text-white dark:bg-[#0E1116]"
-        >
+        <div className="dark min-h-screen bg-white text-white dark:bg-[#0E1116]">
           <Header />
           {children}
         </div>
--- src/components/ui/list-header.tsx
+++ src/components/ui/list-header.tsx
@@ -2,19 +2,22 @@ export const ListHeader = ({
   icon,
   title,
   actionArea,
+  description,
 }: {
   icon: React.ReactNode;
   title: React.ReactNode;
   actionArea?: React.ReactNode;
+  description: string;
 }) => {
   return (
-    <div className="flex h-14 w-full rounded-md rounded-bl-none rounded-br-none border bg-[#171B21] py-2 px-4 text-sm font-medium dark:border-[#383B42] dark:text-slate-100">
-      <div className="flex w-full items-center justify-between">
-        <div className="flex items-center space-x-2">
+    <div className="flex w-full  rounded-md rounded-bl-none rounded-br-none border bg-[#171B21] py-2 px-4 text-sm font-medium dark:border-[#383B42] dark:text-slate-100">
+      <div className="flex w-full flex-col items-start justify-between lg:flex-row lg:items-center">
+        <div className="flex items-center space-x-2 lg:flex-row">
           {icon}
           {title}
         </div>
-        <div className="text-right">{actionArea}</div>
+        <div className="mt-2 flex text-gray-400 lg:mt-0">{description}</div>
+        <div className="hidden text-right lg:flex">{actionArea}</div>
       </div>
     </div>
   );
