0bf124472828b7fda1813457008a725f7dbd0ece
Chris Abdo <chrisabdo@Chriss-Mac-mini.local>
2023-03-06 16:21:39 -0500

    Add array mapping for dropdown menu contents to reduce messy repeating dropdown component in header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -31,15 +31,66 @@ const HeaderConfig: HeaderConfig = {
   ],
 };
 
+const DropdownItems = [
+  {
+    title: "Your Profile",
+    href: "/profile",
+  },
+  {
+    title: "Your Repositories",
+    href: "/repositories",
+  },
+  {
+    title: "Your organizations",
+    href: "/organizations",
+  },
+  {
+    title: "Your projects",
+    href: "/projects",
+  },
+  {
+    title: "Your stars",
+    href: "/stars",
+  },
+  {
+    title: "Your zaps",
+    href: "/zaps",
+  },
+  {
+    title: "Your relays",
+    href: "/relays",
+  },
+  {
+    title: "Your sponsors",
+    href: "/sponsors",
+  },
+  {
+    title: "Upgrade",
+    href: "/upgrade",
+  },
+  {
+    title: "Feature Preview",
+    href: "/feature-preview",
+  },
+  {
+    title: "Help",
+    href: "/help",
+  },
+  {
+    title: "Settings",
+    href: "/settings",
+  },
+];
+
+const PrimaryGitInfo = DropdownItems.slice(0,8)
+const restGitInfo = DropdownItems.slice(8)
+
+
 export const Header = () => {
   return (
     <header className="flex h-14 w-full items-center justify-between bg-[#171B21] px-8">
       <MainNav items={HeaderConfig.mainNav} />
       <div className="hidden items-center md:inline">
-        {/* <Avatar className="h-6 w-6">
-          <AvatarImage src="https://github.com/peerrich.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar> */}
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <div className="flex cursor-pointer">
@@ -54,45 +105,18 @@ export const Header = () => {
             <DropdownMenuLabel>Signed in as XXX</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <span>Your Profile</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span>Your Repositories</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span>Your organizations</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span>Your projects</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span>Your stars</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span>Your zaps</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span>Your relays</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span>Your sponsors</span>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <span>Upgrade</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span>Feature Preview</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span>Help</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <span>Settings</span>
-              </DropdownMenuItem>
+             {PrimaryGitInfo.map((item) => (
+                <DropdownMenuItem key={item.title}>
+                  <span>{item.title}</span>
+                </DropdownMenuItem>
+              ))}
+              <DropdownMenuSeparator />
+              
+              {restGitInfo.map((item) => (
+                <DropdownMenuItem key={item.title}>
+                  <span>{item.title}</span>
+                </DropdownMenuItem>
+              ))}
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
