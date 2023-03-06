9fa086ff28a266f504f5f8da7ced0665a5c5270d
Chris Abdo <chrisabdo@Chriss-Mac-mini.fios-router.home>
2023-03-05 20:43:10 -0500

    Add profile/settings dropdown

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,6 +2,16 @@ import Link from "next/link";
 import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
 
 export const Header = () => {
   return (
@@ -28,10 +38,73 @@ export const Header = () => {
         </nav>
       </div>
       <div className="flex items-center">
-        <Avatar className="h-6 w-6">
+        {/* <Avatar className="h-6 w-6">
           <AvatarImage src="https://github.com/peerrich.png" />
           <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        </Avatar> */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="flex cursor-pointer">
+              <Avatar className="h-6 w-6">
+                <AvatarImage src="https://github.com/peerrich.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <ChevronDown className="mt-1 h-4 w-4 hover:text-white/80" />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuLabel>Signed in as XXX</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <span>Your Profile</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Your Repositories</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Your organizations</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Your projects</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Your stars</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Your zaps</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Your relays</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Your sponsors</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <span>Upgrade</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Try Enterprise</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Feature Preview</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Help</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Settings</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <span>Sign Out</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </header>
   );
