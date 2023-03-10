b52220c4f8ae529df5990041e7674b1d05ef64a6
cypherhoodlum <cypherhoodlum@protonmail.com>
2023-03-10 19:46:46 +0200

    Fix conditional rendering. Prettier. Remove comments & console.logs.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -21,28 +21,24 @@ export default function Login() {
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  /* try entering npub18zx8lw3947pghsgzqv2t0x8pe767sscag5djgj5afr755xkqd97qt530pr */
-
-  // TODO : Unify Login possibilities in one method
   // TODO : setAuthor should be replaced in the future
-  
+
   const handleSubmit = useCallback(async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     await handleLogin();
   }, [setAuthor, router]);
 
-  const handleLogin = useCallback(async (loginType : LoginType = LoginType.default) => {
+  const handleLogin = useCallback(async (loginType: LoginType = LoginType.default) => {
 
-    switch(loginType)
-    {
-      case LoginType.nip07: 
+    switch (loginType) {
+      case LoginType.nip07:
 
-        var hex : string = await window.nostr.getPublicKey()    // nip-07 uses public key to enter,
-        var npub : string = nip19.npubEncode(hex);              // Should consider using hex keys by default, npubs are encoded twice as needed
-        var relays : string[] = Object.keys(await window.nostr.getRelays());
-        
+        var hex: string = await window.nostr.getPublicKey()    // nip-07 uses public key to enter,
+        var npub: string = nip19.npubEncode(hex);              // Should consider using hex keys by default, npubs are encoded twice as needed
+        var relays: string[] = Object.keys(await window.nostr.getRelays());
+
         setAuthor && setAuthor(npub || "");                   // Implement Default relays adds
-        
+
         relays.forEach((url) => {
           addReplay && addReplay(url || "");
           console.log(`added relay ${url}`);
@@ -52,13 +48,10 @@ export default function Login() {
 
       default:
 
-        let cred : string = inputRef.current?.value || '';
-        
+        let cred: string = inputRef.current?.value || '';
+
         var hexRegex = /^[a-fA-F0-9]+$/i; // hex charset
         var npubRegex = /^npub[0-3][qpzry9x8gf2tvdw0s3jn54khce6mua7l]+/i; // bip-36 charset
-
-        console.log(hexRegex.test(cred));
-        console.log()
 
         // Testing regex, if pubkey classic setAuthor, if hex key, nip19 encode then setAuthor
         npubRegex.test(cred) && setAuthor && setAuthor(inputRef.current?.value || "");
@@ -132,7 +125,7 @@ export default function Login() {
                 </Button>
               </div>
             </form>
-            {(typeof(window) !== 'undefined')
+            {(typeof (window.nostr) !== 'undefined')
               ?
               <div className="mt-6">
                 <div className="relative">
@@ -141,7 +134,7 @@ export default function Login() {
                   </div>
                 </div>
                 <div className="mt-6">
-                  <Button className="flex justify-center gap-2 items-center w-full" onClick={() => {handleLogin(LoginType.nip07)}}>
+                  <Button className="flex justify-center gap-2 items-center w-full" onClick={() => { handleLogin(LoginType.nip07) }}>
                     <Puzzle />
                     <p className="text-center">Continue with extension</p>
                   </Button>
@@ -156,14 +149,19 @@ export default function Login() {
                 </div>
                 <div className="mt-6">
                   <div className="relative">
-                    <div className="relative flex justify-center text-sm">
-                      <span className="px-2">Download a nip-07 extension like <a className="underline" href="https://www.getflamingo.org">Flamingo</a></span>
+                    <div className="relative flex justify-center text-center text-sm">
+                      <span className="px-2">
+                        <span className="mr-1">For better security, download a NIP-07 extension like</span>
+                        <a className="underline" href="https://www.getflamingo.org">Flamingo</a>
+                        <span className="ml-1 mr-1">or</span>
+                        <a className="underline" href="https://addons.mozilla.org/en-US/firefox/addon/nos2x-fox/">nos2x-fox</a>.
+                      </span>
                     </div>
                   </div>
                 </div>
-              </div>  
+              </div>
             }
-            
+
           </div>
           <div className="flex justify-center center mt-1">
             <p>
