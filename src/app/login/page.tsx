a165e3c159a33ed92153af0fd4c0ed0690cd3777
Beemo <120607606+b35363@users.noreply.github.com>
2023-03-10 12:34:10 +0000

    retry Some checks were not successful

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -132,7 +132,7 @@ export default function Login() {
                 </Button>
               </div>
             </form>
-            {(typeof(window) !== 'undefined') && window.nostr
+            {(typeof(window) !== 'undefined')
               ?
               <div className="mt-6">
                 <div className="relative">
