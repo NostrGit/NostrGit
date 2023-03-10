7cdf5a05ac4da22b4712219a14cd6768049825d2
e-roy <ericroupe@gmail.com>
2023-03-10 06:20:35 -0500

    feat:ui for [entity]/[repo]/issues page

--- src/app/[entity]/[repo]/issues/page.tsx
+++ src/app/[entity]/[repo]/issues/page.tsx
@@ -1,7 +1,208 @@
+"use client";
+
+import * as React from "react";
+import { useCallback, useEffect, useState } from "react";
+
+import { clsx } from "clsx";
+import {
+  Check,
+  CheckCircle2,
+  ChevronDown,
+  CircleDot,
+  GitMerge,
+  GitPullRequest,
+  MessageSquare,
+  Search,
+} from "lucide-react";
+
+interface IIssueData {
+  id: string;
+  entity: string;
+  repo: string;
+  title: string;
+  number: string;
+  date: string;
+  author: string;
+  tags: string[];
+  taskTotal: number | null;
+  taskCompleted: number | null;
+  linkedPR: number;
+  assignees: string[];
+  comments: number;
+}
+
 export default function RepoIssuesPage() {
+  const [issueStatus, setIssueStatus] = useState<"open" | "closed">("open");
+
+  const [search, setSearch] = useState<string>(`is:open is:issue`);
+
+  const [issues, setIssues] = useState<IIssueData[]>(openData);
+
+  useEffect(() => {
+    if (issueStatus === "open") {
+      setIssues(openData);
+    } else {
+      setIssues([]);
+    }
+  }, [issueStatus]);
+
+  const handleIssueStatusOpen = useCallback(() => setIssueStatus("open"), []);
+  const handleIssueStatusClosed = useCallback(
+    () => setIssueStatus("closed"),
+    []
+  );
+
+  const handleSearch = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) =>
+      setSearch(e.currentTarget.value),
+    []
+  );
+
   return (
-    <>
-      <h1>Issues</h1>
-    </>
+    <section className="sm:px-8 py-6 max-w-6xl m-auto">
+      <div className="md:flex justify-between gap-4 px-4 sm:px-0">
+        <div>filter</div>
+        <label className="relative w-full border-slate-600 text-slate-400 border rounded-md">
+          <span className="sr-only">Search</span>
+          <span className="absolute inset-y-0 left-0 flex items-center px-2">
+            <Search className="h-4 w-4" />
+          </span>
+          <input
+            className="block bg-[#0E1116] w-full h-full rounded-md py-1 pl-9 pr-3 focus:outline-none focus:border-purple-500 focus:ring-purple-500 focus:ring-1 text-sm md:text-base"
+            type="text"
+            value={search}
+            onChange={handleSearch}
+          />
+        </label>
+        <div>labels milestones</div>
+        <div>new issue</div>
+      </div>
+
+      <main>
+        <div className="mt-4">
+          <div className="flex w-full rounded-md rounded-bl-none rounded-br-none border bg-[#171B21] py-2 px-4 dark:border-[#383B42] dark:text-slate-100">
+            <div className="md:flex w-full flex-col text-md py-2 items-start justify-between lg:flex-row lg:items-center">
+              <div className="flex items-center lg:flex-row space-x-4 font-medium">
+                <button
+                  className={clsx("flex text-slate-400 hover:text-slate-200", {
+                    "text-slate-50": issueStatus === `open`,
+                  })}
+                  onClick={handleIssueStatusOpen}
+                >
+                  <CircleDot className="h-5 w-5 mr-2 mt-0.5" /> 3 Open
+                </button>
+                <button
+                  className={clsx("flex text-slate-400 hover:text-slate-200", {
+                    "text-slate-50": issueStatus === `closed`,
+                  })}
+                  onClick={handleIssueStatusClosed}
+                >
+                  <Check className="h-5 w-5 mr-2 mt-0.5" /> 4 Closed
+                </button>
+              </div>
+              <div className="mt-2 flex text-gray-400 lg:mt-0 space-x-6">
+                <span className="flex text-slate-400 hover:text-slate-200 cursor-pointer">
+                  Author <ChevronDown className="h-4 w-4 ml-1 mt-1.5" />
+                </span>
+                <span className="flex text-slate-400 hover:text-slate-200 cursor-pointer">
+                  Label <ChevronDown className="h-4 w-4 ml-1 mt-1.5" />
+                </span>
+                <span className="flex text-slate-400 hover:text-slate-200 cursor-pointer">
+                  Projects <ChevronDown className="h-4 w-4 ml-1 mt-1.5" />
+                </span>
+                <span className="flex text-slate-400 hover:text-slate-200 cursor-pointer">
+                  Milestones <ChevronDown className="h-4 w-4 ml-1 mt-1.5" />
+                </span>
+                <span className="flex text-slate-400 hover:text-slate-200 cursor-pointer">
+                  Assignee <ChevronDown className="h-4 w-4 ml-1 mt-1.5" />
+                </span>
+                <span className="flex text-slate-400 hover:text-slate-200 cursor-pointer">
+                  Sort <ChevronDown className="h-4 w-4 ml-1 mt-1.5" />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="overflow-hidden rounded-md rounded-tr-none rounded-tl-none border border-t-0 dark:border-gray">
+            <ul className="divide-y dark:divide-gray">
+              {issues.map((item) => (
+                <li
+                  key={`${item.id} ${item.entity}`}
+                  className="text-gray-400 grid grid-cols-8 p-2 text-sm hover:bg-[#171B21]"
+                >
+                  <div className="col-span-8 sm:col-span-6">
+                    <div className="sm:flex items-center text-lg font-medium">
+                      <span className="flex">
+                        {issueStatus === "open" ? (
+                          <CircleDot className="h-5 w-5 mr-2 mt-1 text-green-600" />
+                        ) : (
+                          <CheckCircle2 className="h-5 w-5 mr-2 mt-1 text-purple-600" />
+                        )}
+                        <a
+                          className="text-slate-400 hover:text-purple-500"
+                          href="#"
+                        >
+                          {item.entity}/{item.repo}
+                        </a>
+                      </span>
+
+                      <a
+                        className="text-slate-200 hover:text-purple-500 pl-7 sm:pl-3"
+                        href="#"
+                      >
+                        {item.title}
+                      </a>
+                    </div>
+                    <div className="ml-7 text-slate-400">
+                      #{item.number} opened {item.date} by{" "}
+                      <a className="hover:text-purple-500" href="#">
+                        {item.author}
+                      </a>
+                    </div>
+                  </div>
+
+                  <div className="hidden sm:flex col-span-2 text-slate-400 justify-between pt-2 text-right pr-3 no-wrap">
+                    <span className="ml-2 flex hover:text-purple-500 cursor-pointer font-medium">
+                      {item.linkedPR ? (
+                        <>
+                          <GitPullRequest className="h-5 w-5 mr-2" />
+                          {item.linkedPR}
+                        </>
+                      ) : null}
+                    </span>
+                    <span className="ml-2 "></span>
+                    <span className="ml-2 flex hover:text-purple-500 cursor-pointer font-medium">
+                      {item.comments ? (
+                        <>
+                          <MessageSquare className="h-5 w-5 mr-2" />
+                          {item.comments}
+                        </>
+                      ) : null}
+                    </span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </main>
+    </section>
   );
 }
+
+const openData = [
+  {
+    id: "101",
+    entity: "NostrGit",
+    repo: "NostrGit",
+    title: "Make GHA required",
+    number: "59",
+    date: "2 hours ago",
+    author: "pierresisson",
+    tags: [],
+    taskTotal: null,
+    taskCompleted: null,
+    linkedPR: 0,
+    assignees: [],
+    comments: 0,
+  },
+];
