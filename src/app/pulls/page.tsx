7caebf4f24108c5a4e70bcf49780100fa772b6c6
Peer Richelsen <peeroke@gmail.com>
2023-03-12 22:10:09 +0100

    Color slate replaced with zinc (#184)

--- src/app/pulls/page.tsx
+++ src/app/pulls/page.tsx
@@ -82,9 +82,9 @@ export default function PullsPage({}) {
           <button
             type="button"
             className={clsx(
-              "relative w-full rounded-l-md py-1.5 font-semibold text-slate-300 ring-1 ring-inset !ring-gray focus:z-10",
+              "relative w-full rounded-l-md py-1.5 font-semibold text-zinc-300 ring-1 ring-inset !ring-gray focus:z-10",
               {
-                "bg-purple-600 text-slate-50": issueType === `created`,
+                "bg-purple-600 text-zinc-50": issueType === `created`,
               }
             )}
             onClick={handleIssueTypeCreated}
@@ -94,9 +94,9 @@ export default function PullsPage({}) {
           <button
             type="button"
             className={clsx(
-              "relative -ml-px w-full py-1.5 font-semibold text-slate-300 ring-1 ring-inset ring-gray focus:z-10",
+              "relative -ml-px w-full py-1.5 font-semibold text-zinc-300 ring-1 ring-inset ring-gray focus:z-10",
               {
-                "bg-purple-600 text-slate-50": issueType === `assigned`,
+                "bg-purple-600 text-zinc-50": issueType === `assigned`,
               }
             )}
             onClick={handleIssueTypeAssigned}
@@ -106,9 +106,9 @@ export default function PullsPage({}) {
           <button
             type="button"
             className={clsx(
-              "relative -ml-px w-full py-1.5 font-semibold rounded-r-md md:rounded-none text-slate-300 ring-1 ring-inset ring-gray focus:z-10",
+              "relative -ml-px w-full py-1.5 font-semibold rounded-r-md md:rounded-none text-zinc-300 ring-1 ring-inset ring-gray focus:z-10",
               {
-                "bg-purple-600 text-slate-50": issueType === `mentioned`,
+                "bg-purple-600 text-zinc-50": issueType === `mentioned`,
               }
             )}
             onClick={handleIssueTypeMentioned}
@@ -118,9 +118,9 @@ export default function PullsPage({}) {
           <button
             type="button"
             className={clsx(
-              "hidden relative md:block -ml-px w-full rounded-r-md py-1.5 font-semibold text-slate-300 ring-1 ring-inset ring-gray focus:z-10",
+              "hidden relative md:block -ml-px w-full rounded-r-md py-1.5 font-semibold text-zinc-300 ring-1 ring-inset ring-gray focus:z-10",
               {
-                "bg-purple-600 text-slate-50": issueType === `request`,
+                "bg-purple-600 text-zinc-50": issueType === `request`,
               }
             )}
             onClick={handleRequestTypeMentioned}
@@ -129,7 +129,7 @@ export default function PullsPage({}) {
           </button>
         </div>
 
-        <label className="relative w-full text-slate-400">
+        <label className="relative w-full text-zinc-400">
           <span className="sr-only">Search</span>
           <span className="absolute  lg:inset-y-0 lg:left-0 flex items-center px-2">
             <Search className="h-8 w-4" />
@@ -144,20 +144,20 @@ export default function PullsPage({}) {
       </div>
       <main>
         <div className="mt-4">
-          <div className="flex w-full rounded-md rounded-bl-none rounded-br-none border bg-[#171B21] py-2 px-4 dark:border-gray dark:text-slate-100">
+          <div className="flex w-full rounded-md rounded-bl-none rounded-br-none border bg-[#171B21] py-2 px-4 dark:border-gray dark:text-zinc-100">
             <div className="md:flex w-full flex-col text-md py-2 items-start justify-between lg:flex-row lg:items-center">
               <div className="flex items-center lg:flex-row space-x-4 font-medium">
                 <button
-                  className={clsx("flex text-slate-400 hover:text-slate-200", {
-                    "text-slate-50": issueStatus === `open`,
+                  className={clsx("flex text-zinc-400 hover:text-zinc-200", {
+                    "text-zinc-50": issueStatus === `open`,
                   })}
                   onClick={handleIssueStatusOpen}
                 >
                   <CircleDot className="h-5 w-5 mr-2 mt-0.5" /> 3 Open
                 </button>
                 <button
-                  className={clsx("flex text-slate-400 hover:text-slate-200", {
-                    "text-slate-50": issueStatus === `closed`,
+                  className={clsx("flex text-zinc-400 hover:text-zinc-200", {
+                    "text-zinc-50": issueStatus === `closed`,
                   })}
                   onClick={handleIssueStatusClosed}
                 >
@@ -165,13 +165,13 @@ export default function PullsPage({}) {
                 </button>
               </div>
               <div className="mt-2 flex text-gray-400 lg:mt-0 space-x-6">
-                <span className="flex text-slate-400 hover:text-slate-200 cursor-pointer">
+                <span className="flex text-zinc-400 hover:text-zinc-200 cursor-pointer">
                   Visibility <ChevronDown className="h-4 w-4 ml-1 mt-1.5" />
                 </span>
-                <span className="flex text-slate-400 hover:text-slate-200 cursor-pointer">
+                <span className="flex text-zinc-400 hover:text-zinc-200 cursor-pointer">
                   Organization <ChevronDown className="h-4 w-4 ml-1 mt-1.5" />
                 </span>
-                <span className="flex text-slate-400 hover:text-slate-200 cursor-pointer">
+                <span className="flex text-zinc-400 hover:text-zinc-200 cursor-pointer">
                   Sort <ChevronDown className="h-4 w-4 ml-1 mt-1.5" />
                 </span>
               </div>
@@ -193,7 +193,7 @@ export default function PullsPage({}) {
                           <GitMerge className="h-5 w-5 mr-2 mt-1 text-purple-600" />
                         )}
                         <a
-                          className="text-slate-400 hover:text-purple-500"
+                          className="text-zinc-400 hover:text-purple-500"
                           href="#"
                         >
                           {item.entity}/{item.repo}
@@ -201,21 +201,21 @@ export default function PullsPage({}) {
                       </span>
 
                       <a
-                        className="text-slate-200 hover:text-purple-500 pl-7 sm:pl-3"
+                        className="text-zinc-200 hover:text-purple-500 pl-7 sm:pl-3"
                         href="#"
                       >
                         {item.title}
                       </a>
                     </div>
                     {issueStatus === "open" ? (
-                      <div className="ml-7 text-slate-400">
+                      <div className="ml-7 text-zinc-400">
                         #{item.number} opened {item.date} by{" "}
                         <a className="hover:text-purple-500" href="#">
                           {item.author}
                         </a>
                       </div>
                     ) : (
-                      <div className="ml-7 text-slate-400">
+                      <div className="ml-7 text-zinc-400">
                         #{item.number} by{" "}
                         <a className="hover:text-purple-500" href="#">
                           {item.author}
@@ -225,7 +225,7 @@ export default function PullsPage({}) {
                     )}
                   </div>
 
-                  <div className="hidden sm:flex col-span-2 text-slate-400 justify-between pt-2 text-right pr-3 no-wrap">
+                  <div className="hidden sm:flex col-span-2 text-zinc-400 justify-between pt-2 text-right pr-3 no-wrap">
                     <span className="ml-2 "></span>
                     <span className="ml-2 flex hover:text-purple-500 cursor-pointer font-medium">
                       {item.comments ? (
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const TextArea = React.forwardRef<HTMLTextAreaElement, InputProps>(
     return (
       <textarea
         className={cn(
-          "flex h-8 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900",
+          "flex h-8 w-full rounded-md border border-zinc-300 bg-transparent py-2 px-3 text-sm placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-zinc-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-700 dark:text-zinc-50 dark:focus:ring-zinc-400 dark:focus:ring-offset-zinc-900",
           className
         )}
         ref={ref}
