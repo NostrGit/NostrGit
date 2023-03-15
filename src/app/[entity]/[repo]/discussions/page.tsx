e13850b7259fd41743afd56921dfad61b398f124
Peer Richelsen <peer@cal.com>
2023-03-15 23:55:00 +0100

    fixed colors, added new event hash

--- src/app/[entity]/[repo]/discussions/page.tsx
+++ src/app/[entity]/[repo]/discussions/page.tsx
@@ -1,7 +1,6 @@
 // TODO: turn all of those imported scripts into more lightweight react components
 // change 503941a9939a4337d9aef7b92323c353441cb5ebe79f13fed77aeac615116354 to the correct event (once posted)
-
-import Script from 'next/script'
+import Script from "next/script";
 
 export default function RepoDiscussionsPage() {
   return (
@@ -15,17 +14,30 @@ export default function RepoDiscussionsPage() {
         id="comments-init"
         strategy="lazyOnload"
         dangerouslySetInnerHTML={{
-          __html: `let relay = comments_init("503941a9939a4337d9aef7b92323c353441cb5ebe79f13fed77aeac615116354")`,
+          __html: `let relay = comments_init("10488mc62vzcf7yw8x0pavewdh5vmm5j4r279l9x43rss8tjzdc0s2lsrfp")`,
         }}
       />
-      <div id="comments" className="mt-4 max-w-5xl">loading comments...</div>
-      <div className="border border-gray rounded-md w-full max-w-5xl mb-32">
-        <div className="flex text-sm bg-[#171B21] rounded-md rounded-bl-none rounded-br-none border-gray h-11 border-b pt-2 pl-2">
-          <div className="relative -bottom-px border-t border-l border-r bg-[#0F1116] rounded-md rounded-bl-none rounded-br-none border-gray h-full max-w-min flex items-center p-4">Write</div>
-          <div className="relative -bottom-px text-slate-400 h-full max-w-min flex items-center p-4">Preview</div>
+      <div id="comments" className="mt-4 max-w-5xl min-h-[500px]">
+        loading comments...
+      </div>
+      <div className="border border-lightgray rounded-md w-full max-w-5xl mb-32">
+        <div className="flex text-sm bg-[#171B21] rounded-md rounded-bl-none rounded-br-none border-lightgray h-11 border-b pt-2 pl-2">
+          <div className="relative -bottom-px border-t border-l border-r bg-[#0F1116] rounded-md rounded-bl-none rounded-br-none border-lightgray h-full max-w-min flex items-center p-4">
+            Write
+          </div>
+          <div className="relative -bottom-px text-slate-400 h-full max-w-min flex items-center p-4">
+            Preview
+          </div>
         </div>
-        <a href="https://snort.social/e/note12qu5r2vnnfpn0kdw77ujxg7r2dzped0tu7038lkh0t4vv9g3vd2qjxr9c7" target="_blank" className="p-2 block">
-          <textarea rows={4} className="w-full bg-black border border-gray rounded-md min-h-full flex h-full" />
+        <a
+          href="https://snort.social/e/note10488mc62vzcf7yw8x0pavewdh5vmm5j4r279l9x43rss8tjzdc0s2lsrfp"
+          target="_blank"
+          className="p-2 block"
+        >
+          <textarea
+            rows={4}
+            className="w-full bg-black border border-lightgray rounded-md min-h-full flex h-full"
+          />
         </a>
       </div>
     </>
